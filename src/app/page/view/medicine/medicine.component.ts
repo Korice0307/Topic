bd189658f76daaaf644edd4d70f2643b591ae24f
Korice0307 <hung20020307@gmail.com>
2024-12-19 14:35:40 +0800

    medicine commit

--- src/app/page/view/medicine/medicine.component.ts
+++ src/app/page/view/medicine/medicine.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-medicine',
   standalone: false,
-  
   templateUrl: './medicine.component.html',
   styleUrl: './medicine.component.scss'
 })
 export class MedicineComponent {
+constructor(private router: Router) { }
 
+  selectedIndex = 0; // 默認顯示第一個標籤（心跳）
+
+  // 根據需要可以動態設置選中的標籤
+  setActiveTab(index: number): void {
+    this.selectedIndex = index;
+  }
+  goHome() {
+    this.router.navigate(['/']);
+  }
 }
